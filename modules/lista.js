65b88d03ede85cc77abb7400e915a752c4bf14d8
Rafael Costa Braga <c111807@corp.caixa.gov.br>
2016-12-15 18:53:29 -0200

    Gravando lista no db

--- modules/lista.js
+++ modules/lista.js
@@ -2,12 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-    fornecedores: [{label: String, value: {name: String, status: Boolean}}],
-    operacao: [{label: String, value: {name: String, status: Boolean}}],
-    categoria: [{label: String, value: {name: String, status: Boolean}}],
-    produto: [{label: String, value: {name: String, status: Boolean}}],
-    transportadora: [{label: String, value: {name: String, status: Boolean}}]
+    label: {type: String},
+    name: {type: String},
+    status: {type: String, default: true}
 });
-
-
 module.exports = mongoose.model('Lista', schema);
--- routes/lista.js
+++ routes/lista.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var Lista = require('../modules/lista');
 
 /* GET home page. */
 router.post('/item', function(req, res, next) {
   var label = req.body.label;
   var lista = req.body.lista;
 
+  for (let i = 0; i < lista.length; i++) {
+    var ll = new Lista({
+      label: label,
+      name: lista[i]
+    })
+    ll.save(function(err, result) {})
+  }
   res.status(201).json({
-    type: "success"
+    label: label
   })
 })
 
